b38cf3d37e400edc4e165a1284275694231cc2f5
Harsh Mehta <harshmehta.srm2002@gmail.com>
2024-09-18 20:44:48 +0530

    api not working

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,15 +6,16 @@ function Body() {
   const fetchData = async () => {
     const response = await fetch(
       "https://dev.deepthought.education/assets/uploads/files/files/others/ddugky_project.json",
-      {
-        mode: "no-cors",
-        headers: new Headers({
-          "Access-Control-Allow-Origin": "http://127.0.0.1:5173",
-          "Access-Control-Allow-Methods": "GET",
-          "Access-Control-Allow-Headers": "application/json",
-        }),
-      }
+      // {
+      { mode: "no-cors" }
+      // headers: new Headers({
+      //   "Access-Control-Allow-Origin": "http://127.0.0.1:5173",
+      //   "Access-Control-Allow-Methods": "GET",
+      //   "Access-Control-Allow-Headers": "application/json",
+      // }),
+      // }
     );
+    console.log(response);
     const data = await response.json();
     setData(data);
   };
@@ -30,15 +31,15 @@ function Body() {
         marginRight: "calc(10% + 46px)",
       }}
     >
-      <div>
-        {/* {data.map((item, index) => (
+      {/* <div>
+        {data.map((item, index) => (
           <div className="grid grid-cols-2" key={index}>
             <div>
-              <h1>{item.assets.asset_title}</h1>
+              <h1>{data.assets}</h1>
             </div>
           </div>
-        ))} */}
-      </div>
+        ))}
+      </div> */}
     </div>
   );
 }
